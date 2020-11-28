4e4e250f6c1a447045b8d5b23af19e9f68900b67
utrape <pablosud025@gmail.com>
2020-11-28 13:22:32 -0300

    bug fixed

--- main.py
+++ main.py
@@ -31,6 +31,6 @@ while True:
         window['go'].update(visible=True)
     
     if event == 'go':
-        url = use.returnProfilelLink()
+        url = use.returnProfileLink()
         webbrowser.open(url)
 
