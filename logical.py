61f3c4253f76f6f5d438aa3a5c6aa20351e6a7cc
utrape <pablosud025@gmail.com>
2020-11-29 08:39:48 -0300

    add features

--- logical.py
+++ logical.py
@@ -1,25 +1,29 @@
-import requests
-import json
+import requests, json
 
 class UseGitHubAPI:
     
-    def takeInfo(self, user):
+    def _takeProfileInfo(self, user):
         url = f'https://api.github.com/users/{user}'
         try:
             with requests.get(url) as response:
-                self._takedJson = json.loads(response.text)
-            self.selectFields(self._takedJson)
+                self._takenProfileJson = json.loads(response.text)
+        except:
+            ...
+    
+    
+    def returnProfileInfo(self, user=''):
+        try:
+            self._takeProfileInfo(user)
+            self._selectProfileFields(self._takenProfileJson)
             self.readable = ''
-            for k, v in self.selectedFields.items():
-                if v == '':
-                    v = None
-                self.readable = self.readable + f'{k}: {v} \n\n'     
+            self.readable = self.turnReadable(self._selectedProfileFields)
             return self.readable
         except:
             return 'not found'
-        
-    def selectFields(self, info):
-        self.selectedFields = {
+    
+    
+    def _selectProfileFields(self, info):
+        self._selectedProfileFields = {
             'Name': info['name'],
             'Bio': info['bio'],
             'Email': info['email'],
@@ -39,4 +43,55 @@ class UseGitHubAPI:
         
         
     def returnProfileLink(self):
-        return self.selectedFields['URL']
+        return self._selectedProfileFields['URL']
+    
+    
+    def _takeRepositoriesInfo(self):
+        if self._takenProfileJson == None:
+            ...
+        else:
+            url = self._takenProfileJson['url'] + '/repos'
+            with requests.get(url) as response:
+                self._takenRepositoriesJson = json.loads(response.text)
+            self._takenRepositoriesJson
+            
+    def _selectRepositoriesFields(self, info, pos=0):
+        if pos>len(info)-1:
+            ...
+        else:    
+            info = info[pos]
+            self._selectedRepositoriesFields = {
+                'Name': info['name'],
+                'Description': info['description'],
+                'Language': info['language'],
+                'Private': info['private'],
+                'URL': info['html_url'],
+                'Default branch': info['default_branch'],
+                'was it a fork': info['fork'],
+                'Created at': info['created_at'],
+                'Updated at': info['updated_at'],
+                'Stargazers': info['stargazers_count'],
+                'Watchers': info['watchers'],
+                'Git url': info['git_url'],
+                'SSH url': info['ssh_url'],
+                'Clone url': info['clone_url']}
+        
+
+    def returnRepositorieInfo(self, pos):
+        try:
+            self._takeRepositoriesInfo()
+            self._selectRepositoriesFields(info=self._takenRepositoriesJson, pos=pos)
+            self.readable = self.turnReadable(self._selectedRepositoriesFields)
+            return self.readable
+        except:
+            return 'not found'
+        
+    
+    def turnReadable(self, dictionary):
+        self.turningReadable = ''
+        for k, v in dictionary.items():
+            if v == '':
+                v = None
+            self.turningReadable = self.turningReadable + f'{k}: {v} \n\n'     
+        return self.turningReadable
+            
--- main.py
+++ main.py
@@ -8,13 +8,16 @@ sg.theme('DarkBlue')
 layout = [
     [
         sg.Text('User Name: '), sg.In(key='input', text_color='Black'), 
-        sg.B('Search', key='search')
+        sg.B('Search', key='search'),
+        sg.B('view repositories', key='repositories', disabled=True)
     ],
     [
-        sg.Multiline(key='test', size=(65, 30), text_color='Black')
+        sg.Multiline(key='print_info', size=(90, 30), text_color='Black'),
     ],
     [
-        sg.B('go to profile', key='go', visible=False)
+        sg.B('previous', key='previous', disabled=True),
+        sg.B('go to profile', key='go', disabled=True),  
+        sg.B('next', key='next', disabled=True)
     ]]
 
 window = sg.Window('GitHub Search', layout)
@@ -26,11 +29,37 @@ while True:
         break
     
     if event == 'search':
+        
         searching = values['input'].replace(' ', '')
-        window['test'].update(use.takeInfo(searching))
-        window['go'].update(visible=True)
+        window['print_info'].update(use.returnProfileInfo(searching))
+        window['go'].update(disabled=False)
+        window['repositories'].update(disabled=False)
+        window['previous'].update(disabled=True)
+        window['next'].update(disabled=True)
     
     if event == 'go':
-        url = use.returnProfileLink()
-        webbrowser.open(url)
+        try:
+            url = use.returnProfileLink()
+            webbrowser.open(url)
+        except:
+            ...
+            
+    if event == 'repositories':
+        repositorieNow = 0
+        window['print_info'].update(use.returnRepositorieInfo(repositorieNow))
+        window['previous'].update(disabled=False)
+        window['next'].update(disabled=False)
+        
+        
+    if event == 'next':
+        repositorieNow +=1
+        window['print_info'].update(use.returnRepositorieInfo(repositorieNow))
+    
+    if event == 'previous':
+        repositorieNow -=1
+        if repositorieNow>0:
+            window['print_info'].update(use.returnRepositorieInfo(repositorieNow))
+            ...
+        else:
+            repositorieNow +=1
 
